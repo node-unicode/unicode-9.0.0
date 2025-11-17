49b3d769d4a52adb21fd17cc2c1fd5c47889e288
Mathias Bynens <mathias@qiwi.be>
2025-11-17 10:54:39 +0100

    Release v1.6.15

---
+++ Sentence_Break/STerm/ranges.js
@@ -0,0 +1,1 @@
+module.exports=require('../../decode-ranges.js')('ghAcAwVIAiUAizAgqCj1AlpBwbjBsVADBsEAjFBjLAEAk5BlhDitBBBjaBhABu7BICw3jAMAnEA3T7AkNBjiACAl9BhVBheAiXBiSCiPBj4A1JpBioAcAhAAwrzBwXuBh0Dh-CiABFAPBhXBABhrAmgBl0BEOhoBj4CwUBB04qBiEAhABKA1FZAx3nA')
